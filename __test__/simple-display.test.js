22b62ce9b4fcc4774524215a9db913d2bc591032
Leonardo Merlin <leonardomerlin@gmail.com>
2018-04-23 17:26:54 -0300

    initial commit

---
+++ __test__/simple-display.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const simpleDisplay = require('../lib/core');
+
+describe.skip('SimpleDisplay', () => {
+  test('should reject inputs outside of range [1..9]', () => {});
+  test('should work with "1"', () => {});
+  test('should work with "2"', () => {});
+  test('should work with "3"', () => {});
+  test('should work with "4"', () => {});
+  test('should work with "5"', () => {});
+  test('should work with "6"', () => {});
+  test('should work with "7"', () => {});
+  test('should work with "8"', () => {});
+  test('should work with "9"', () => {});
+});
