4947956e2ff53d96c6a3368cf0fcca9d8117e9d5
Leonardo Merlin <leonardomerlin@gmail.com>
2018-04-23 17:31:47 -0300

    test: handle exceptions

--- __test__/simple-display.test.js
+++ __test__/simple-display.test.js
@@ -1,16 +1,34 @@
-'use strict';
+'use strict'
 
-const simpleDisplay = require('../lib/core');
+const simpleDisplay = require('../lib/core')
 
-describe.skip('SimpleDisplay', () => {
-  test('should reject inputs outside of range [1..9]', () => {});
-  test('should work with "1"', () => {});
-  test('should work with "2"', () => {});
-  test('should work with "3"', () => {});
-  test('should work with "4"', () => {});
-  test('should work with "5"', () => {});
-  test('should work with "6"', () => {});
-  test('should work with "7"', () => {});
-  test('should work with "8"', () => {});
-  test('should work with "9"', () => {});
-});
+describe('SimpleDisplay', () => {
+  test('should reject inputs outside of range [1..9]', () => {
+    const messageError = 'The input must be a number between 1 and 9.'
+    // test limits
+    expect(() => {
+      simpleDisplay(0)
+    }).toThrowError(messageError)
+    expect(() => {
+      simpleDisplay(10)
+    }).toThrowError(messageError)
+
+    // test negative values
+    expect(() => {
+      simpleDisplay(-1)
+    }).toThrowError(messageError)
+    expect(() => {
+      simpleDisplay(-10)
+    }).toThrowError(messageError)
+  })
+
+  test.skip('should work with "1"', () => {})
+  test.skip('should work with "2"', () => {})
+  test.skip('should work with "3"', () => {})
+  test.skip('should work with "4"', () => {})
+  test.skip('should work with "5"', () => {})
+  test.skip('should work with "6"', () => {})
+  test.skip('should work with "7"', () => {})
+  test.skip('should work with "8"', () => {})
+  test.skip('should work with "9"', () => {})
+})
